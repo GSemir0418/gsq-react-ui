6269bbef86ab96270a02f2e9c5abf2db4414e81b
GSemir <845217811@qq.com>
2021-05-19 14:51:30 +0800

    index.tsx可以打包为index.js了！

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+//node模块导出定义
+module.exports = {
+    //生产环境
+    mode:'production',
+    //入口
+    entry: {
+        index: './lib/index.tsx'
+    },
+    //输出的配置
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        //输出库的名字
+        library: 'gsq-react-ui',
+        //输出库的格式，封装成什么样的模块定义
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,//如果是以.tsx或.ts结尾的，就用下面的loader翻译成js文件
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    }
+}
